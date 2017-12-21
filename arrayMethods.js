4f0cd5f97ba09c1334ed3aad849b63fb9e0a2bbe
levi <levi@levibrooke.com>
2017-12-20 19:19:47 -0800

    Complete pop()

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,15 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+
+// push()
+
+var firstName = "Levi";
+var lastName = "Porter";
+var nameArray = [];
+
+nameArray.push(firstName, lastName);
+
+genericNumberArray.push(6, 7, 8 , 9, 10);
+
+// pop()
